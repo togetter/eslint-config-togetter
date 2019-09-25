f3a1f87eabc654363d9a35dadde7df7a5aa79eb6
e_ntyo <entyo.contact@gmail.com>
2019-09-25 20:03:06 +0900

    .eslintrc.yml -> .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: "babel-eslint",
+  plugins: ["prettier"],
+  extends: ["airbnb/base", "plugin:prettier/recommended"],
+  rules: {
+    "prettier/prettier": "error",
+    "max-len": [2, 120, 4],
+    "class-methods-use-this": [0],
+    "no-underscore-dangle": [1],
+    "no-mixed-operators": [1],
+    "no-plusplus": [
+      2,
+      {
+        allowForLoopAfterthoughts: true
+      }
+    ],
+    "no-restricted-globals": [0],
+    "function-paren-newline": [0],
+    "prefer-destructuring": [1],
+    "padded-blocks": [0],
+    "getter-return": [1],
+    "object-curly-newline": [1]
+  }
+};
