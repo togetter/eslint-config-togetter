9d60a6dd948e2107bdb78c743be2a0532f607102
yositosi <yositosi@gmail.com>
2019-10-11 01:18:39 +0900

    未設定だとTogetterでErrorになる項目のみ追加した

--- index.js
+++ index.js
@@ -3,22 +3,11 @@ module.exports = {
   extends: ["airbnb/base", "plugin:prettier/recommended"],
   rules: {
     "prettier/prettier": "error",
-    "max-len": [2, 120, 4],
-    "class-methods-use-this": [0],
     "no-underscore-dangle": [1],
-    "no-mixed-operators": [1],
-    "no-plusplus": [
-      2,
-      {
-        allowForLoopAfterthoughts: true
-      }
-    ],
+    "class-methods-use-this": [0],
     "no-restricted-globals": [0],
-    "function-paren-newline": [0],
+    "max-classes-per-file": [0],
     "prefer-destructuring": [1],
-    "padded-blocks": [0],
-    "getter-return": [1],
-    "object-curly-newline": [1],
-    "max-classes-per-file": [0]
+    "getter-return": [1]
   }
 };
